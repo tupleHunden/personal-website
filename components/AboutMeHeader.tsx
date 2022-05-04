ba5889d0866725b4317e24aa10ab31e5e04d53a0
RJ <rj@pop-os.localdomain>
2022-05-03 23:50:40 -0400

    Fixed typo

--- components/AboutMeHeader.tsx
+++ components/AboutMeHeader.tsx
@@ -11,7 +11,7 @@ function AboutMeHeader() {
     <div className="ml-10 mt-10 p-5 container max-w-xl mx-auto ">
       <h1 className="text-4xl">Hello, I{"'"}m RJ.</h1>
       <p className="mt-3 bg-orange-300 p-2.5 text-xl rounded-lg">
-        I{"'"}m a fullstack developer and military veteran living in the Boston.
+        I{"'"}m a fullstack developer and military veteran living in Boston.
         I currently work at Audible building web applications using TypeScript,
         React, and Python on AWS Serverless.
       </p>
