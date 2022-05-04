b4b89667f0adf72b731d8c420744008f7a0c2c6c
RJ <rj@pop-os.localdomain>
2022-05-03 23:37:03 -0400

    Updated SEO and padding

--- components/AboutMeHeader.tsx
+++ components/AboutMeHeader.tsx
@@ -1,14 +1,45 @@
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 function AboutMeHeader() {
   return (
-    <div className="ml-10 mt-10 container max-w-xl mx-auto ">
+    <div className="ml-10 mt-10 p-5 container max-w-xl mx-auto ">
       <h1 className="text-4xl">Hello, I{"'"}m RJ.</h1>
       <p className="mt-3 bg-orange-300 p-2.5 text-xl rounded-lg">
         I{"'"}m a fullstack developer and military veteran living in the Boston.
         I currently work at Audible building web applications using TypeScript,
         React, and Python on AWS Serverless.
       </p>
+      <div className="flex flex-row ml-1 mt-5">
+        <a
+          href="https://github.com/tupleHunden"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faGithub} /> GitHub
+        </a>
+        <a
+          className="ml-10"
+          href="https://twitter.com/tupleHunden"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faTwitter} /> Twitter
+        </a>
+        <a
+          className="ml-10"
+          href="https://linkedin.com/in/rj-smith"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+        </a>
+      </div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import AboutMeHeader from "../components/AboutMeHeader";
-import SocialIcons from "../components/SocialIcons";
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Head>
+        <title>RJ Smith | Fullstack Developer</title>
+        <meta name="description" content="Personal website for RJ Smith." />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <AboutMeHeader />
-      <SocialIcons />
     </div>
   );
 };
